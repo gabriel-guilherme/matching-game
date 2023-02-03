6ee622ec8d322a88653ca54c79277171ddd8f11d
quebramundos <gabriel.guilherme.131@ufrn.edu.br>
2023-02-03 11:15:58 -0300

    init

---
+++ src/components/ArrrowDown/index.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+function ArrowDown(currentPlayer=1){
+
+    return /*html*/`
+    <img class="arrow-down"
+    data-currentPlayer="${currentPlayer}"
+    src="images/IconArrowDown.png"
+    alt="Icone de uma seta para baixo">
+    `
+}
+
+export default ArrowDown;
